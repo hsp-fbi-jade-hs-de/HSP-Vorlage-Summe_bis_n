d2c1b04699bcf5057aeb7f8b96571e8a2beff50d
Wilko Lühring <wilko.luehring@student.jade-hs.de>
2020-10-10 13:26:40 +0200

    UnitTest fix

---
+++ Summe_bis_n.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Summe_bis_n.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Summe_bis_n.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0730567c-eb38-43d8-bf84-7f9adf5865d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Summe_bis_n.UnitTests/Summe_bis_n_UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Summe_bis_n;
+
+namespace Summe_bis_n.UnitTests
+{
+    [TestClass]
+    public class Summe_bis_n_UnitTests
+    {
+        [TestMethod]
+        public void Summe_bis_n_Test1()
+        {
+            Summe_bis_n prg = new Summe_bis_n();
+
+            var summe = prg.Summe_bis_n_Methode(123);
+
+            Assert.AreEqual(7503, summe);
+        }
+    }
+}
