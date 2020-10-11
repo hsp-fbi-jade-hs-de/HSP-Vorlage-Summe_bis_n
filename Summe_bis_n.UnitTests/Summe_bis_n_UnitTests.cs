9aae78a8e5ab3147db48b4ec6e5a208a3d1a2791
Wilko Lühring <wilko.luehring@student.jade-hs.de>
2020-10-11 10:33:13 +0200

    hotfix + neue UnitTests

--- Summe_bis_n.UnitTests/Summe_bis_n_UnitTests.cs
+++ Summe_bis_n.UnitTests/Summe_bis_n_UnitTests.cs
@@ -12,9 +12,29 @@ namespace Summe_bis_n.UnitTests
         {
             Summe_bis_n prg = new Summe_bis_n();
 
+            var summe = prg.Summe_bis_n_Methode(3);
+
+            Assert.AreEqual(6, summe);
+        }
+
+        [TestMethod]
+        public void Summe_bis_n_Test2()
+        {
+            Summe_bis_n prg = new Summe_bis_n();
+
+            var summe = prg.Summe_bis_n_Methode(1);
+
+            Assert.AreEqual(1, summe);
+        }
+
+        [TestMethod]
+        public void Summe_bis_n_Test3()
+        {
+            Summe_bis_n prg = new Summe_bis_n();
+
             var summe = prg.Summe_bis_n_Methode(123);
 
-            Assert.AreEqual(7503, summe);
+            Assert.AreEqual(7626, summe);
         }
     }
 }
